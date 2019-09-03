0ace34bacc2ff3e357aa558d5205e42f2b2ef1b4
Peter Williams <pedroeldiablo@yahoo.com>
2019-09-03 13:44:31 +0100

    adding demo css-in-js with material/ui styles

--- src/PaletteList.js
+++ src/PaletteList.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom'
+import MiniPalette from './MiniPalette';
+import { Link } from 'react-router-dom';
 
 class PaletteList extends Component {
     render() {
         const { palettes } = this.props;
         return (
             <div className="PaletteList">
+                <MiniPalette />
                 <h1>React Palettes</h1>
                 {palettes.map(palette => (
                     <Link to={`/palette/${palette.id}`}>{palette.paletteName}</Link>
