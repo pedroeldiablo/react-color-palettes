b2a75fa2362ccc3a93d747cc8232434585a66382
Peter Williams <pedroeldiablo@yahoo.com>
2019-09-06 22:59:01 +0100

    use classNames to improve readablity and avoid having to use string interpolation to apply optional classes

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { CopyToClipboard } from 'react-copy-to-clipboard';
+import classNames from "classnames";
 import styles from './styles/ColorBoxStyles';
 import { withStyles } from '@material-ui/styles';
 
@@ -24,10 +25,11 @@ class ColorBox extends Component {
                 <div style={{backgroundColor: backgroundColor}} className={classes.ColorBox}>
                     <div 
                         style={{backgroundColor: backgroundColor}} 
-                        className={`${classes.copyOverlay} ${copied  && classes.showOverlay}`} 
+                        className= {classNames(classes.copyOverlay, {[classes.showOverlay]:copied})}
                         />
                     <div 
-                        className={`${classes.copyMessage} ${copied  && classes.showCopyMessage}`}
+                        className={classNames(classes.copyMessage, {[classes.showCopyMessage]:copied})}
+                       
                     >
                         <h1>Copied</h1>
                         <p className={classes.copyText}>
