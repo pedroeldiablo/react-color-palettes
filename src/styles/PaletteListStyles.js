1982091d490fcd945fb63cb1b81eea7a76ca29c7
Peter Williams <pedroeldiablo@yahoo.com>
2019-09-06 18:36:31 +0100

    fade out deleted palettes using react-transition-groups to control animation

--- src/styles/PaletteListStyles.js
+++ src/styles/PaletteListStyles.js
@@ -1,6 +1,15 @@
 import sizes from './sizes';
 
 export default {
+    "@global": {
+        ".fade-exit": {
+            opacity: 1
+        },
+        ".fade-exit-active": {
+            opacity: 0,
+            transition: "opacity 500ms ease-out"
+        },
+    },
     root: {
         backgroundColor: "blue",
         minHeight: "100vh",
